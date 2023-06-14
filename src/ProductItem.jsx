cf59a865e7289cce8ac3d128f4b1420922ba9f03
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-06-15 03:54:11 +0400

    style changes

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -2,12 +2,16 @@ import react from "react";
 
 const ProductItem = ({productImg, productName, productDescription, productPrice, productKey, deleteProduct}) => {
     return (
-        <div key = {productKey}>
-            <h3 className="name">{productName}</h3>
+        <div key = {productKey} className = "productItem">
+            <figure>
             <img src = {productImg}/>
+            </figure>
+            <section className="details">
+            <h1 className="name">{productName}</h1>
             <p className="description">{productDescription}</p>
-            <h3 className="price">{productPrice}</h3>
-            <button className="deleteProduct" onClick={deleteProduct}>Delete Product</button>
+            <h1 className="price">{productPrice}</h1>
+            <button className="deleteProductBtn" onClick={deleteProduct}>Delete Product</button>
+            </section>
         </div>
     )
 }
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,10 +3,22 @@ import ProductItem from "./ProductItem";
 
 const ProductList = () => {
   const [products, setProducts] = useState(() => {
-    return JSON.parse(localStorage.getItem("products")) || [{productImg: "https://cdn.shopify.com/s/files/1/1002/7150/products/New-Mockups---no-hanger---TShirt-Yellow.jpg?v=1639657077", 
-                                             productName: "T-Shirt",
-                                             productDescription: "T-Shirt", 
-                                             productPrice: "5$" }, 
+    return JSON.parse(localStorage.getItem("products")) || [{ productImg: "https://st.mngbcn.com/rcs/pics/static/T4/fotos/outfit/S20/47010043_30-99999999_01.jpg?ts=1667559794423&imwidth=502&imdensity=2", 
+                                                              productName: "Natural fiber basket",
+                                                              productDescription: "100% cattail. Natural fibers. Braided design. Oval design. Two handles.", 
+                                                              productPrice: "US$ 69.99" }, 
+                                                              {productImg: "https://st.mngbcn.com/rcs/pics/static/T3/fotos/S20/37086704_37_B.jpg?ts=1666337770051&imwidth=502&imdensity=2", 
+                                                               productName: "Fringed blanket 5512x748 in",
+                                                               productDescription: "1Plain fabric. Soft texture. Fringed finish. Very warm fabric. Ideal for cold seasons. An essential item for your living room or bedroom.", 
+                                                               productPrice: "US$ 59.99" }, 
+                                                               {productImg: "https://st.mngbcn.com/rcs/pics/static/T4/fotos/outfit/S20/47055873_52-99999999_01.jpg?ts=1678896241731&imwidth=502&imdensity=2", 
+                                                               productName: "Striped cushion case",
+                                                               productDescription: "With this cotton garment you support our investment in the mission of Better Cotton. ", 
+                                                               productPrice: "US$ 15.99" }, 
+                                                               {productImg: "https://st.mngbcn.com/rcs/pics/static/T4/fotos_alt/S20/47001255_08_02_D1.jpg?ts=1667562241000&imwidth=502&imdensity=2", 
+                                                               productName: "Natural fiber tray 25x18cm",
+                                                               productDescription: "Size: 25x18. Natural fibers. 100% rattan. Accessories for your home. ", 
+                                                               productPrice: "US$ 19.99" }, 
                                              ]
                                             });
 
@@ -44,14 +56,8 @@ const [newProduct, setNewProduct] = useState({
 
   return (
     <div>
-      <h2>Product List</h2>
-      <div className = "productList">
-        {products.map((product, index) => (
-          <ProductItem productKey = {index} productImg = {product.productImg} productName = {product.productName} productDescription = {product.productDescription} productPrice = {product.productPrice} deleteProduct = {deleteProduct}/>
-        ))}
-      </div>
+      <h2 className = "headName"> MANGO </h2>
 
-      
       <div className = "addProductInput">
         <input
           type="text"
@@ -84,6 +90,14 @@ const [newProduct, setNewProduct] = useState({
         />
         <button onClick={handleAddProduct}>Add Product</button>
     </div>
+
+      <div className = "productList">
+        {products.map((product, index) => (
+          <ProductItem productKey = {index} productImg = {product.productImg} productName = {product.productName} productDescription = {product.productDescription} productPrice = {product.productPrice} deleteProduct = {() => deleteProduct(index)}/>
+        ))}
+      </div>
+
+      
     </div>
   );
 };
